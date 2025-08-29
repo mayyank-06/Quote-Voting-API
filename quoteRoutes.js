bfb3a79ba745dff5cd79f7287a5bb1b1fd774ee4
MAYANK VERMA <mayankvermaa83@gmail.com>
2025-08-29 17:42:42 +0530

    Update quoteRoutes.js

--- quoteRoutes.js
+++ quoteRoutes.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const {
+  getQuotes,
+  submitQuote,
+  upvoteQuote,
+  downvoteQuote,
+  deleteQuote,
+} = require('../controllers/quoteController');
+
+const router = express.Router();
+
+router.get('/', getQuotes);
+router.post('/', submitQuote);
+router.put('/:id/upvote', upvoteQuote);
+router.put('/:id/downvote', downvoteQuote);
+router.delete('/:id', deleteQuote);
+
+module.exports = router;
 
