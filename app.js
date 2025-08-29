d8d52ec64e237a98edc5820e298426c319d0ec10
MAYANK VERMA <mayankvermaa83@gmail.com>
2025-08-29 17:42:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+const quoteRoutes = require('./routes/quoteRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/quotes', quoteRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
